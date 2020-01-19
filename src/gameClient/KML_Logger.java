5be930e19191657643e168e0d8724dc5e2abede3
NDM-123 <57434608+NDM-123@users.noreply.github.com>
2020-01-19 02:57:44 +0200

    KML Update - prints to file

--- src/gameClient/KML_Logger.java
+++ src/gameClient/KML_Logger.java
@@ -3,6 +3,10 @@ package gameClient;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
@@ -10,89 +14,76 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import Server.game_service;
+import algorithms.Graph_Algo;
+import dataStructure.DGraph;
 import dataStructure.graph;
 import dataStructure.node_data;
-import de.micromata.opengis.kml.v_2_2_0.Document;
-import de.micromata.opengis.kml.v_2_2_0.Kml;
-import de.micromata.opengis.kml.v_2_2_0.Placemark;
-import de.micromata.opengis.kml.v_2_2_0.TimeStamp;
+import de.micromata.opengis.kml.v_2_2_0.*;
+import de.micromata.opengis.kml.v_2_2_0.Icon;
+import utils.Point3D;
+
 
 public class KML_Logger {
+    static StringBuilder Kfile = new StringBuilder();
+   private static int scenarioInt;
 
-  public static boolean KML_Save(graph gra, List<String> robots,List<String> fruits){
-        try {
-            Kml kl = new Kml();
-            Document document = kl.createAndSetDocument().withName("Map.scenario");
-            for (int i = 0; i < robots.size(); i++) {
-                TimeStamp t = new TimeStamp();
-                t.setWhen(new Date().toString());
-                Placemark placemark = document.createAndAddPlacemark();
-                String j = robots.get(i);
-                placemark.withTimePrimitive(t).createAndSetPoint().addToCoordinates(j);
-            }
-            for (int i = 0; i < fruits.size(); i++) {
-                TimeStamp t = new TimeStamp();
-                t.setWhen(new Date().toString());
-                Placemark placemark = document.createAndAddPlacemark();
-                String j = fruits.get(i);
-                placemark.withTimePrimitive(t).createAndSetPoint().addToCoordinates(j);
-            }
-            Collection<node_data> ver = gra.getV();
-            Iterator it = ver.iterator();
-            while(it.hasNext()){
-                TimeStamp t = new TimeStamp();
-                t.setWhen(new Date().toString());
-                Placemark placemark = document.createAndAddPlacemark();
-//                String j = get(it.next().);
-//                placemark.withTimePrimitive(t).createAndSetPoint().addToCoordinates(j);
 
-            }
-//            try {
-//                System.out.println("hi");
-//         //       kl.marshal(new File(file));
-//            } catch (FileNotFoundException ex) {
-//                ex.printStackTrace();
-//                //     Logger.getLogger(KMLFile.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-
-        }catch(Exception e){
-            e.printStackTrace();
+    public static boolean KML_Save(Scenario s) {
+        scenarioInt = s.num;
+        List<String> robots = s.game.getRobots();
+        List<String> fruits = s.game.getFruits();
+        graph gra = s.gr;
+        Graph_Algo ga = new Graph_Algo();
+        ga.init(gra);
+        Date date = new Date();
+        DateFormat ymd = new SimpleDateFormat("yyyy-MM-dd");
+        DateFormat hms = new SimpleDateFormat("HH:mm:ss");
+        String year = ymd.format(date);
+        String hour = hms.format(date);
+        String fDate = year + "T" + hour + "Z";
+        Kfile.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<kml xmlns=\"http://earth.google.com/kml/2.2\">\n" +
+                "  <Document>\n" +
+                "    <name>The maze of Waze 2</name>");
+        Iterator it = robots.iterator();
+        while (it.hasNext()) {
+            Robot r = new Robot((String) it.next());
+            Kfile.append("<Placemark> \n<TimeStamp>\n<when> " + fDate + "\n</when>\n<Point>\n<coordinates> (" + r.getPos().x() + "," + r.getPos().y() + ")\n</coordinates>\n</Point>\n");
+            Kfile.append("<Style>\n<IconStyle>\n<scale>1.0</scale>\n<Icon>\n<href>rob.png</href>\n</Icon>\n</IconStyle>\n</Style>\n");
         }
+        Iterator it2 = fruits.iterator();
+        while (it2.hasNext()) {
+            Fruit f = new Fruit((String) it2.next());
+            if (f.getType() == 1) {             //apple
+                Kfile.append("<Placemark> \n<TimeStamp>\n<when> " + fDate + "\n</when>\n<Point>\n<coordinates> (" + f.getPos().x() + "," + f.getPos().y() + ")\n</coordinates>\n</Point>\n");
+                Kfile.append("<Style>\n<IconStyle>\n<scale>1.0</scale>\n<Icon>\n<href>apple2.png</href>\n</Icon>\n</IconStyle>\n</Style>\n");
+            } else {                           //banana
+                Kfile.append("<Placemark> \n<TimeStamp>\n<when> " + fDate + "\n</when>\n<Point>\n<coordinates> (" + f.getPos().x() + "," + f.getPos().y() + ")\n</coordinates>\n</Point>\n");
+                Kfile.append("<Style>\n<IconStyle>\n<scale>1.0</scale>\n<Icon>\n<href>banana.png</href>\n</Icon>\n</IconStyle>\n</Style>\n");
+            }
 
-
-
-        /**
-         * @param args
-         */
-//        public static void main(String[] args) {
-//            // TODO Auto-generated method stub
-//            String file = "1.kml";
-//            test1(file);
-//        }
-//
-//        public static void test1(String file) {
-//            try {
-//                final Kml kml = new Kml();
-//                Document document = kml.createAndSetDocument().withName("Map.scenario");
-//                for(int i=0;i<10;i++) {
-//                    TimeStamp t = new TimeStamp();
-//                    t.setWhen(new Date().toString());
-//                    Placemark placemark = document.createAndAddPlacemark();
-//                    placemark.withTimePrimitive(t).createAndSetPoint().addToCoordinates(35,32+i/1000.0,i);
-//                }
-//                try {
-//                    kml.marshal(new File(file));
-//                } catch (FileNotFoundException ex) {
-//                    ex.printStackTrace();
-//                    //     Logger.getLogger(KMLFile.class.getName()).log(Level.SEVERE, null, ex);
-//                }
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//
-      return false;
         }
-
+        Kfile.append("</Document>\n</kml>");
+       return  saveFile();
     }
+
+       private static boolean saveFile(){
+
+        boolean flag = false;
+        try {
+
+            FileWriter writer = new FileWriter(scenarioInt+".kml",true);
+            writer.write(String.valueOf(Kfile));
+            writer.close();
+            flag=true;
+        }catch(Exception e){
+          e.printStackTrace();
+          flag = false;
+        }
+
+           return flag;
+    }
+}
+
 
